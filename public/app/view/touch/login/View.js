8a4a7272da4b44235cab58637a2d53663f66997e
wvanheemstra <wvanheemstra@icloud.com>
2013-09-12 20:12:24 +0100

    Updated

--- public/app/view/touch/login/View.js
+++ public/app/view/touch/login/View.js
@@ -22,100 +22,100 @@ Ext.define("Skin.view.touch.login.View", {
         ],
 		style: Ext.Viewport.getOrientation() == "portrait" ? "width:320px;height:480px;margin:auto;-moz-border-radius:15px;border-radius:15px;" : "width:480px;height:320px;margin:auto;-moz-border-radius:15px;border-radius:15px;",
 		baseCls: 'x-box',		
-        items: [
+		items: [
 //            {
 //                xtype: "image",
 //                src: Ext.Viewport.getOrientation() == "portrait" ? "../../../img/login.png" : "../../../img/login-small.png",
 //                style: Ext.Viewport.getOrientation() == "portrait" ? "width:80px;height:80px;margin:auto" : "width:40px;height:40px;margin:auto"
 //            },
-            {
-                xtype: "fieldset",
-                itemId: "company",
-                title: "Your Company",
+			{
+				xtype: "fieldset",
+				itemId: "company",
+				title: "Your Company",
 				height: 187, // make this flexible
-                items: [
-                    {
-                        xtype: "textfield",
-                        itemId: "usernameTextField",
-                        name: "usernameTextField",
-                        required: true,
-                        plugins: [
-                            {
-                                type: "localization",
-                                method: "setPlaceHolder",
-                                key: "login.username"
-                            }
-                        ]
-                    },
-                    {
-                        xtype: "passwordfield",
-                        itemId: "passwordTextField",
-                        name: "passwordTextField",
-                        required: true,
-                        plugins: [
-                            {
-                                type: "localization",
-                                method: "setPlaceHolder",
-                                key: "login.password"
-                            }
-                        ]
-                    },
-                    {
-                        xtype: "checkboxfield",
-                        itemId: "keepmeloggedinCheckboxField",
-                        name: "keepmeloggedinCheckboxField",
-                        required: false,
-                        checked: false,
-                        plugins: [
-                            {
-                                type: "localization",
-                                method: "setLabel",
-                                key: "login.keepmeloggedin"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                xtype: "label",
-                plugins: [
-                    {
-                        type: "localization",
-                        method: "setText",
-                        key: "login.passwordHint"
-                    }
-                ]
-            },
-            {
-                xtype: "button",
-                itemId: "logInButton",
-                ui: "neutral", // WAS "action",
-                padding: 10,
+				items: [
+					{
+						xtype: "textfield",
+						itemId: "usernameTextField",
+						name: "usernameTextField",
+						required: true,
+						plugins: [
+							{
+								type: "localization",
+								method: "setPlaceHolder",
+								key: "login.username"
+							}
+						]
+					},
+					{
+						xtype: "passwordfield",
+						itemId: "passwordTextField",
+						name: "passwordTextField",
+						required: true,
+						plugins: [
+							{
+								type: "localization",
+								method: "setPlaceHolder",
+								key: "login.password"
+							}
+						]
+					},
+					{
+						xtype: "checkboxfield",
+						itemId: "keepmeloggedinCheckboxField",
+						name: "keepmeloggedinCheckboxField",
+						required: false,
+						checked: false,
+						plugins: [
+							{
+								type: "localization",
+								method: "setLabel",
+								key: "login.keepmeloggedin"
+							}
+						]
+					}
+				]
+			},
+			{
+				xtype: "label",
+				plugins: [
+					{
+						type: "localization",
+						method: "setText",
+						key: "login.passwordHint"
+					}
+				]
+			},
+			{
+				xtype: "button",
+				itemId: "logInButton",
+				ui: "neutral", // WAS "action",
+				padding: 10,
 				margin: 10,
-                plugins: [
-                    {
-                        type: "localization",
-                        method: "setText",
-                        key: "login.submit"
-                    }
-                ]
-            },
-            {
-                xtype: "label",
-                itemId: "logInFailedLabel",
-                name: "logInFailedLabel",
-                hidden: true,
-                hideAnimation: "fadeOut",
-                showAnimation: "fadeIn",
-                style: "color:#990000;margin:5px 10px;",
-                plugins: [
-                    {
-                        type: "localization",
-                        method: "setText",
-                        key: "login.loginFailed"
-                    }
-                ]
-            }
-        ]
+				plugins: [
+					{
+						type: "localization",
+						method: "setText",
+						key: "login.submit"
+					}
+				]
+			},
+			{
+				xtype: "label",
+				itemId: "logInFailedLabel",
+				name: "logInFailedLabel",
+				hidden: true,
+				hideAnimation: "fadeOut",
+				showAnimation: "fadeIn",
+				style: "color:#990000;margin:5px 10px;",
+				plugins: [
+					{
+						type: "localization",
+						method: "setText",
+						key: "login.loginFailed"
+					}
+				]
+			}
+		]
     }
 });
